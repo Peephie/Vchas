38e154862928add420c520452c5862fde77beec8
IlliaOstrovskyi <aleanrenuar@gmail.com>
2024-07-15 20:36:17 +0300

    Initial commint

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: [
+		"./index.html",
+		"./src/**/*.{js,ts,jsx,tsx}",
+	],
+  theme: {
+    extend: {
+			fontFamily:{
+				sans: ['Railway', 'sans-serif']
+			},
+			colors: {
+				'beige': '#FAF1EB',
+				'peach': '#E5A599',
+				'coral': '#D57F6E',
+				'red': '#B63820',
+				'cherry': '#97230D',
+				'black': '#1E1C1E',
+			},
+			gridTemplateColumns: {
+				'50/50': '49% 49%',
+			}
+		},
+  },
+  plugins: [],
+}
+
