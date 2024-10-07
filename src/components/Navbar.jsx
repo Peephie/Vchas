02c49805fb2e9ffa7734637127db52545af6bc05
Dmytro Samoilov <dima.samoilov48491@gmail.com>
2024-10-07 23:38:38 +0300

    Navbar feature
    Changed:
    - Navbar component to work in theme context. In build NavbarSearch and NavbarContacts
    - NavLinks
    
    Created:
    - NavbarModal in order to wrap new modals that should work from Navbar into that compoennt
    - NavbarSearch that is wrapped in NavbarModal
    - NavbarContacts that is wrapped in NavbarModal

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,122 @@
-import React, {useContext} from 'react';
-import Links from './NavLinks';
+import { useContext, useState, useEffect, useRef } from 'react';
 import { ThemeContext } from '../assets/hooks/ThemeContext';
+import { Link } from "react-router-dom";
+import anime from 'animejs';
+import Links from './NavbarLinks';
+import Modal from './NavbarModal';
+import Search from './NavbarSearch';
+import Contacts from './NavbarContacts';
+
 
 const NavBar = () => {
-  const { theme } = useContext(ThemeContext);
-  const textColor = theme !== 'variant-beige-to-cherry' ? theme : 'text-cherry';
   const navbarStorage = [
-    {label: 'Слова', value:'#'},
-    {label: 'Контакти', value:'#'},
-    {label: 'Пошук', value:'#'}
+    {label: 'Слова', value:'/words'},
+    {label: 'Випадкове', value:'/randomWords'},
   ];
 
+  const { theme } = useContext(ThemeContext);
+  const textColorLogoNav = theme !== 'variant-beige-to-cherry' ? theme : 'text-cherry';
+  const textColorNavBar = theme !== 'variant-beige-to-cherry' ? theme : 'variant-inverse';
+
+  const [searchVisible, setSearchVisible] = useState(false);
+  const [contactsVisible, setContactsVisible] = useState(false);
+  
+  const searchRef = useRef(null);
+  const contactsRef = useRef(null);
+
+  const toggleContacts = () => {
+    if (contactsVisible) {
+      anime({
+        targets: contactsRef.current,
+        translateY: '-100%',
+        duration: 500,
+        easing: 'easeInOutQuad',
+        complete: () => setContactsVisible(false),
+      });
+    } else {
+      setContactsVisible(true);
+    }
+  };
+
+  const toggleSearch = () => {
+    console.log('%csrc/components/Navbar.jsx:27 object', 'color: #007acc;', 'HERE');
+    if (searchVisible) {
+      anime({
+        targets: searchRef.current,
+        translateY: '-100%',
+        duration: 500,
+        easing: 'easeInOutQuad',
+        complete: () => setSearchVisible(false),
+      });
+    } else {
+      setSearchVisible(true);
+    }
+  };
+
+  useEffect(() => {
+    if (contactsVisible) {
+      anime({
+        targets: contactsRef.current,
+        translateY: '-100%',
+        duration: 0,
+        complete: () => {
+          anime({
+            targets: contactsRef.current,
+            translateY: '0%', 
+            duration: 500,
+            easing: 'easeInOutQuad',
+          });
+        },
+      });
+    }
+  }, [contactsVisible]);
+
+  useEffect(() => {
+    if (searchVisible) {
+      anime({
+        targets: searchRef.current,
+        translateY: '-100%',
+        duration: 0, 
+        complete: () => {
+          anime({
+            targets: searchRef.current,
+            translateY: '0%', 
+            duration: 500,
+            easing: 'easeInOutQuad',
+          });
+        },
+      });
+    }
+  }, [searchVisible]);
+  
   return (
-    <div className={`flex justify-between px-16 py-10 ${theme}`}>
-      <span className={`italic text-4xl ${textColor}`}>ВЧАС</span>
-      <Links links={navbarStorage} className={theme}/>
-    </div>
+    <nav className={`${theme} flex justify-between px-16 py-10`}>
+      <Link className={`${textColorLogoNav} italic text-4xl cursor-pointer`} to='/'>
+        ВЧАС
+      </Link>
+      <div className="flex justify-between gap-x-5">
+        <Links links={navbarStorage} className={theme}/>
+        <span className={`${textColorNavBar}`}>/</span>
+        <div className={`${textColorNavBar} cursor-pointer`} onClick={toggleContacts}>
+          Контакти
+        </div>
+        <span className={`${textColorNavBar}`}>/</span>
+        <div className={`${textColorNavBar} cursor-pointer`} onClick={toggleSearch}>
+          Пошук
+        </div>
+      </div>
+
+      {contactsVisible && 
+        <Modal theme={theme} onClose={toggleContacts} ref={contactsRef}>
+          <Contacts></Contacts>
+        </Modal>
+      }
+      {searchVisible && 
+        <Modal theme={theme} onClose={toggleSearch} ref={searchRef}>
+          <Search></Search>
+        </Modal>
+      }
+    </nav>
   )
 }
 
---
+++ src/components/NavbarSearch.jsx
@@ -0,0 +1,83 @@
+import { useState, forwardRef } from 'react'
+import { Link } from "react-router-dom";
+import SearchPointer from '../assets/vectors/Search/search_result_point.svg';
+import words from '../assets/data/words.json';
+
+const NavbarSearch = forwardRef(({ theme, onClose }, ref) => {
+  const wordsData = words;
+
+  const textColorSearch = theme !== 'variant-beige-to-cherry' ? theme : 'variant-inverse';
+  const textColorPrimaryWord = theme !== 'variant-beige-to-cherry' || theme === 'variant-base' ? 'text-coral' : 'text-peach';
+  const textColorRelatedWord = theme !== 'variant-beige-to-cherry' || theme === 'variant-base' ? 'text-peach' : 'text-coral';
+  const colorBullet = textColorPrimaryWord === 'text-coral' ? 'stroke-coral' : 'stroke-peach';
+  
+  const [searchValue, changeSearchValue] = useState('');
+  const [filteredWordsData, setFilteredWordsData] = useState([]);
+
+  const handleChangeSearchValue = (event) => {
+    
+    const value = event.target.value;
+    changeSearchValue(value);
+
+    if (!value) {
+      setFilteredWordsData([]);
+      return;
+    }
+
+    const valueLower = value.toLowerCase();
+    const matchingData = wordsData
+    .filter(word => word.primaryWord.toLowerCase().includes(valueLower) || word.relatedWord.toLowerCase().includes(valueLower))
+    .sort((a, b) => {
+      const primaryIndexA = a.primaryWord.toLowerCase().indexOf(valueLower);
+      const primaryIndexB = b.primaryWord.toLowerCase().indexOf(valueLower);
+      
+      if (primaryIndexA !== -1 && primaryIndexB === -1) return -1;
+      if (primaryIndexA === -1 && primaryIndexB !== -1) return 1;
+
+      const relatedIndexA = a.relatedWord.toLowerCase().indexOf(valueLower);
+      const relatedIndexB = b.relatedWord.toLowerCase().indexOf(valueLower);
+
+      if (primaryIndexA !== -1 && primaryIndexB !== -1) return primaryIndexA - primaryIndexB;
+      return relatedIndexA - relatedIndexB;
+    })
+    .slice(0, 3);
+    console.log('%csrc/components/Navbar.jsx:72 filteredWordsData', 'color: #007acc;', filteredWordsData);
+    setFilteredWordsData([...matchingData]);
+  };
+  
+  return (
+    <div className='flex flex-col flex-nowrap justify-center items-center h-5/6	mb-20' ref={ref}>
+      <div className='min-w-search flex flex-row justify-center items-end'>
+        <input className={`${textColorSearch} search-input mr-5`} 
+          type="text" 
+          value={searchValue} 
+          onChange={handleChangeSearchValue}
+        />
+        <span className=' text-search-word'>ПОШУК</span>
+      </div>
+      <div className="mt-20 min-w-search min-h-56 flex flex-col justify-start items-start pl-2.5 gap gap-10">
+        {filteredWordsData.map((word) => (
+          <Link className="flex flex-row flex-nowrap justify-start items-center text-2xl" 
+            key={word.id} 
+            to={`words/${word.id}`} 
+            onClick={onClose}
+          >
+            <div className='mr-4'><SearchPointer class={`${colorBullet}`}/></div> 
+            <div>
+              <span className={`${textColorPrimaryWord} font-raleway-italic mr-3`}>
+                {word.primaryWord.toLowerCase()}&nbsp;
+              </span> 
+              <span className={`${textColorRelatedWord} font-raleway`}>
+                [ {word.relatedWord.toLowerCase()} ]
+              </span>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  )
+});
+
+NavbarSearch.displayName = 'NavbarSearch';
+
+export default NavbarSearch
