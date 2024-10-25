a9b44138545997e7641be7393cb3be59254bb7fb
Dmytro Samoilov <dima.samoilov48491@gmail.com>
2024-10-25 17:11:29 +0300

    Updated BorderButton
    - BorderButton updated styles to make cursor pointer always

--- src/components/BorderedButton.jsx
+++ src/components/BorderedButton.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const BorderedButton = ({theme, variant, label}) => {
-  const buttonDefaultStyles = 'border font-cormorant-infant-semiBold-italic transition-all duration-500 uppercase ';
+  const buttonDefaultStyles = 'cursor-pointer border font-cormorant-infant-semiBold-italic transition-all duration-500 uppercase ';
 
   const buttonSizeVariants = {
     default: 'text-3xl px-10 py-3',
