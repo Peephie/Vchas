5bbc767f460becbc5b4d69ba059f5a45812c2406
Peephie <aleanrenuar@gmail.com>
2024-11-12 22:13:19 +0200

    Added scaling animation on main screen

--- src/components/MainScreen.jsx
+++ src/components/MainScreen.jsx
@@ -1,9 +1,41 @@
-import React, {useContext} from 'react';
+import React, { useContext, useRef } from 'react';
+import anime from 'animejs';
 import MainHero from './MainHero'
 import { ThemeContext } from '../assets/hooks/ThemeContext';
 
 const MainScreen = () => {
+  const randomWordsRef = useRef(null);
+  const allWordsRef = useRef(null);
+  const heroes = [randomWordsRef, allWordsRef];
   const { theme } = useContext(ThemeContext);
+
+  addHoverListeners();
+
+  function addHoverListeners() {
+    
+    heroes.forEach((heroRef) => {
+      heroRef?.current?.addEventListener('mouseenter', ({target}) => onEnterHero(target), false);
+      heroRef?.current?.addEventListener('mouseleave', ({target}) => onLeaveHero(target), false);
+    });
+  }
+
+  function onEnterHero(el) {
+    scaleHero(el, 1.1, 800, 200);
+  }
+  
+  function onLeaveHero(el) {
+    scaleHero(el, 1.0, 600, 100);
+  }
+
+  function scaleHero(el, scale, duration, elasticity) {
+    anime.remove(el);
+    anime({
+      targets: el,
+      scale: scale,
+      duration: duration,
+      elasticity: elasticity
+    });
+  }
 
 	return (
     <div className={`${theme} grid grid-cols-50/50`}>
@@ -14,6 +46,7 @@ const MainScreen = () => {
         linkValue='#'
         bgImage='ink-image'
         link='/randomWords'
+        ref={randomWordsRef}
       />
       <MainHero 
         heading='Всі Слова'
@@ -22,6 +55,7 @@ const MainScreen = () => {
         linkValue='#'
         bgImage='paper-image'
         link='/words'
+        ref={allWordsRef}
       />
     </div>
 	)
